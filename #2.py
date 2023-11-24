0327a438645c1e6acf6df1e1e439fe817e89927c
anton03013 <antongnguyen0@gmail.com>
2023-11-24 08:32:05 +0700

    Ok

---
+++ #2.py
@@ -0,0 +1,18 @@
+#jkdsfhhbkjndkjg
+dfg
+dfg
+dfg
+dfg
+dfg
+defghk[hasattrfgjh
+       fgj
+       fgnf
+       gd
+       
+       d
+       fgd
+       FloatingPointErrord
+       FloatingPointErrordfg
+       d
+       FloatingPointErrordg
+       ]
