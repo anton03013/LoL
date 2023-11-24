102d09b174af3403d1e3a8e1a3233571a8113c69
anton03013 <85305920+anton03013@users.noreply.github.com>
2023-11-24 07:47:57 +0700

    Add files via upload

---
+++ #1.py
@@ -0,0 +1,27 @@
+#1
+a = int(input())
+s = 0
+for i in range(1,a+1):
+    s = s + i**3
+print(s)
+
+#2
+d = int(input())
+s3 = 0
+for i in range(1,d+1):
+    s3 = s3 + i**2
+print(s3)
+
+#3
+b = int(input())
+s1 = 0
+for i in range(1,b+1):
+    s1 = s1 + 1/i
+print(s1)
+
+#4
+c = int(input())
+s2 = 0
+for i in range(1,c+1):
+    s = s + 1/(i * (i + 1))
+print(s)
